a971bf613043ef10724ae721be56a43c12cedaf6
Edoardo Zanoni <edoardo.zanoni@gmail.com>
2017-06-04 11:02:05 +0200

    - First iteration of Home/Index - Removed useless ViewModel IndexData

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,17 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MetaTesina.Data;
+using MetaTesina.Models;
+using MetaTesina.Helpers;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
 
 namespace MetaTesina.Controllers
-{
+{   
     [AllowAnonymous]
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public HomeController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
-            return View();
+            _context = context;
+            _userManager = userManager;
+        }
+        
+        public async Task<IActionResult> Index()
+        {
+            var data = _context.Article
+                            .Include(d => d.Category)
+                            .Include(d => d.ArticleLinkImg)
+                            .OrderByDescending(d => d.ArticleCreateDate);
+
+            return View(await data.ToListAsync());
         }
 
         public IActionResult About()
